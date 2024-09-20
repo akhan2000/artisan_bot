a5fc304416b5fbcba6b55d92f47250cbbc8083ce
akhan2000 <ak556@duke.edu>
2024-09-19 21:01:59 -0400

    setting db url as env variable for backend hosting

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,13 @@
 # app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+# Use the DATABASE_URL provided by Railway
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
